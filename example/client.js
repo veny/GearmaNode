8854822e7e62cf97d10d07cb2b436106e35140e5
veny <vaclav.sykora@gmail.com>
2013-07-11 21:10:43 +0200

    added validation to (Worker)Job#reportStatus

--- example/client.js
+++ example/client.js
@@ -7,27 +7,27 @@ var gearmanode = require('../lib/gearmanode'),
 
 
 // simplest sample for README.md - foreground job waiting for completition
-// var client = gearmanode.client(); // by default expects job server on localhost:4730
-// client.submitJob({ name: 'reverse', payload: 'hello world!' }, function(err, job) { // by default foreground job with normal priority
-// //client.submitJob({ name: 'reverse', payload: 'žluťoučký kůň' }, function(err, job) { // by default foreground job with normal priority
-//     job.on('complete', function() {
-//         console.log("RESULT >>> " + job.response);
-//         client.close();
-//     });
-// })
-
-
-// simplest sample for README.md - foreground job receiving status update
-var client = gearmanode.client();
-client.submitJob({ name: 'sleep', payload: '3' }, function(err, job) {
-    job.on('status', function(result) {
-        console.log('STATUS >> ' + util.inspect(result));
-    });
+var client = gearmanode.client(); // by default expects job server on localhost:4730
+client.submitJob({ name: 'reverse', payload: 'hello world!' }, function(err, job) { // by default foreground job with normal priority
+//client.submitJob({ name: 'reverse', payload: 'žluťoučký kůň' }, function(err, job) { // by default foreground job with normal priority
     job.on('complete', function() {
         console.log("RESULT >>> " + job.response);
         client.close();
     });
 })
+
+
+// simplest sample for README.md - foreground job receiving status update
+// var client = gearmanode.client();
+// client.submitJob({ name: 'sleep', payload: '3' }, function(err, job) {
+//     job.on('status', function(result) {
+//         console.log('STATUS >> ' + util.inspect(result));
+//     });
+//     job.on('complete', function() {
+//         console.log("RESULT >>> " + job.response);
+//         client.close();
+//     });
+// })
 
 
 // var timeout = 3000;
--- example/worker.js
+++ example/worker.js
@@ -3,26 +3,26 @@ var gearmanode = require('../lib/gearmanode');
 
 
 // simplest sample for README.md
-// var worker = gearmanode.worker();
-// worker.addFuntion('reverse', function (job) {
-//     var rslt = job.payload.split("").reverse().join("");
-//     job.workComplete(rslt);
-// });
-
-
 var worker = gearmanode.worker();
-worker.addFuntion('sleep', function (job) {
-    var seconds = new Number(job.payload);
-    var cnt = 0;
-    var tmo = function() {
-        if (cnt < seconds) {
-            cnt ++;
-            console.log('== sleep: idx=' + cnt + ', ' + job.toString());
-            job.reportStatus(cnt, seconds);
-            setTimeout(tmo, 1000);
-        } else {
-            job.workComplete();
-        }
-    }
-    tmo();
+worker.addFuntion('reverse', function (job) {
+    var rslt = job.payload.split("").reverse().join("");
+    job.workComplete(rslt);
 });
+worker.removeFuntion('reverse');
+
+// var worker = gearmanode.worker();
+// worker.addFuntion('sleep', function (job) {
+//     var seconds = new Number(job.payload);
+//     var cnt = 0;
+//     var tmo = function() {
+//         if (cnt < seconds) {
+//             cnt ++;
+//             console.log('== sleep: idx=' + cnt + ', ' + job.toString());
+//             job.reportStatus(cnt, seconds);
+//             setTimeout(tmo, 1000);
+//         } else {
+//             job.workComplete();
+//         }
+//     }
+//     tmo();
+// });
