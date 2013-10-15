55f3a991f95799eab4cadf0ea99c3fc7aff24d6f
veny <vaclav.sykora@gmail.com>
2013-10-15 22:30:36 +0200

    working on load balancing

--- test/test-load-balancing.js
+++ test/test-load-balancing.js
@@ -4,6 +4,10 @@ var should     = require('should'),
     Sequence   = require('../lib/gearmanode/load-balancing').Sequence;
 
 describe('load-balancing', function() {
+    var lb;
+    beforeEach(function() {
+        lb = new Sequence(2);
+    });
 
 
     describe('#LBStrategy', function() {
@@ -11,7 +15,6 @@ describe('load-balancing', function() {
 
         describe('#badOne', function() {
             it('should store given index with timestamp', function() {
-                var lb = new Sequence(2);
                 Object.keys(lb.badNodes).length.should.equal(0);
                 lb.badOne(1);
                 Object.keys(lb.badNodes).length.should.equal(1);
@@ -19,9 +22,17 @@ describe('load-balancing', function() {
                 lb.badNodes[1].should.be.an.instanceof(Date);
             })
             it('should ignore index bigger than node count', function() {
-                var lb = new Sequence(2);
                 Object.keys(lb.badNodes).length.should.equal(0);
                 lb.badOne(2);
+                Object.keys(lb.badNodes).length.should.equal(0);
+            })
+            it('should accept only number', function() {
+                Object.keys(lb.badNodes).length.should.equal(0);
+                lb.badOne('0');
+                Object.keys(lb.badNodes).length.should.equal(0);
+                lb.badOne(null);
+                Object.keys(lb.badNodes).length.should.equal(0);
+                lb.badOne(false);
                 Object.keys(lb.badNodes).length.should.equal(0);
             })
         })
@@ -33,19 +44,16 @@ describe('load-balancing', function() {
 
         describe('#nextIndex', function() {
             it('should return the same index if everything OK', function() {
-                var lb = new Sequence(2);
                 lb.nextIndex().should.equal(0);
                 lb.nextIndex().should.equal(0);
                 lb.nextIndex().should.equal(0);
             })
             it('should return next index if the current fails', function() {
-                var lb = new Sequence(2);
                 lb.badOne(0);
                 lb.nextIndex().should.equal(1);
                 lb.nextIndex().should.equal(1);
             })
             it('should return null if all nodes fails', function() {
-                var lb = new Sequence(2);
                 lb.nextIndex().should.equal(0);
                 lb.badOne(0);
                 lb.badOne(1);
